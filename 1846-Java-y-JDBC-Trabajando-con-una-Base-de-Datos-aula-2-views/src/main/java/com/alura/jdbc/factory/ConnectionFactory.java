c225d7d46e33ae6204f964f0b77a15a075b806d4
Fernando Rojas <juanrojas.jfrc@gmail.com>
2023-08-30 01:16:43 -0500

    Primer Commit

---
+++ 1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -0,0 +1,12 @@
+package com.alura.jdbc.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	public Connection recuperaConexion() throws SQLException {
+		return DriverManager.getConnection("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC", "root", "1001");
+	}
+}
